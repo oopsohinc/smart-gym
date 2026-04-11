aa810a8e40baafb18b4fbf357824877cc3db0c19
oopsohinc <minhnguyen.160479@gmail.com>
2026-04-11 13:30:30 +0700

    chore: cập nhật server bootstrap với subscription scheduler

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const app = require('./src/app');
 const { connectDatabase } = require('./src/config/database');
 const { env } = require('./src/config/env');
+const { startSubscriptionLifecycleScheduler } = require('./src/services/subscriptionLifecycle.service');
 
 async function bootstrap() {
   await connectDatabase();
 
-  app.listen(env.PORT, () => {
+  const lifecycleTimer = startSubscriptionLifecycleScheduler({
+    intervalMs: env.SUBSCRIPTION_LIFECYCLE_INTERVAL_MS
+  });
+
+  const shutdown = () => {
+    clearInterval(lifecycleTimer);
+  };
+
+  process.on('SIGINT', shutdown);
+  process.on('SIGTERM', shutdown);
+
+  app.listen(env.PORT, "0.0.0.0", () => {
     console.log(`SV Gym backend running on port ${env.PORT}`);
   });
 }
