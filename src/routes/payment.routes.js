dc59a842b297db06c90e0742ce89ae388cac6f54
oopsohinc <minhnguyen.160479@gmail.com>
2026-05-26 17:18:19 +0700

    fix: lỗi redirect vnpay với phân quyền mới

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -6,11 +6,10 @@ const {
 } = require('../controllers/payment.controller');
 const { authenticate } = require('../middleware/authenticate');
 const { authorize } = require('../middleware/authorize');
-const { USER_ROLES } = require('../constants/enums');
 
 const router = express.Router();
 
-router.post('/vnpay/create', authenticate, authorize(USER_ROLES.MEMBER), createVnpayPayment);
+router.post('/vnpay/create', authenticate, authorize('member.*'), createVnpayPayment);
 router.get('/vnpay/return', handleVnpayReturn);
 router.get('/vnpay/ipn', handleVnpayIpn);
 router.post('/vnpay/ipn', handleVnpayIpn);
